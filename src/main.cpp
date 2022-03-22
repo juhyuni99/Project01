cfb38102306aa4a6e1ae905702bce08e406896d4
juhyuni99 <leemss1@naver.com>
2022-03-22 16:36:52 +0900

    LED Arduino

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+const int  LED = 2;
 void setup() {
+  pinMode(LED, OUTPUT);
   // put your setup code here, to run once:
 }
 
 void loop() {
+  digitalWrite(LED, HIGH);
+  delay(500);
+  digitalWrite(LED, LOW);
+  delay(500);
   // put your main code here, to run repeatedly:
 }
